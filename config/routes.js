91d610bc7a312dafdc7ef6e161d8b49eaa77cbc0
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-04-05 13:01:22 -0400

    Fixed (non) error in register, completed Login by moving the generateToken fn to authenticate, able to make get request to dad jokes

--- config/routes.js
+++ config/routes.js
@@ -2,13 +2,14 @@ const axios = require('axios');
 const bcrypt = require('bcryptjs');
 
 
-const { authenticate } = require('../auth/authenticate');
+
+const { authenticate, generateToken } = require('../auth/authenticate');
 
 const db = require('../database/dbConfig')
 
 module.exports = server => {
   server.post('/api/register', register);
-  server.post('/api/login', login);
+  server.post('/api/login', login, generateToken);
   server.get('/api/jokes', authenticate, getJokes);
 };
 
@@ -18,10 +19,10 @@ const hash = bcrypt.hashSync(user.password, 10)
 user.password = hash;
 db('users')
 .insert(user)
-.then(ids => {
-  const id = ids[0]
-  db('users')
-  .where({id})
+ .then(ids => {
+ const id = ids[0]
+   db('users')
+   .where({id})
   .then(users => {
     res.status(201).json(users)
   })
@@ -32,8 +33,32 @@ db('users')
 }
 
 function login(req, res) {
+  let { username, password } = req.body;
+  console.log(username)
+  db('users')
+  .where({username})
+  .first()
+  .then(user => {
+    console.log(user)
+   if (user && bcrypt.compareSync(password, user.password)) {
+     const token = generateToken(user)
  
+     res.status(200).json({
+       message: `Hi ${user.username}!`,
+       token,
+       
+     });
+     
+   } else {
+     res.status(401).json({ message: 'Invalid Credentials' });
+   }
+ })
+  .catch(error => {
+    res.status(500).json(error)
+  })
 }
+
+
 
 
 
