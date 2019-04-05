67c106490fd5d8c25f1ff4501aa0c9e9657783f7
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-04-05 11:51:41 -0400

    Register, getting error

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+
 
 const { authenticate } = require('../auth/authenticate');
+
+const db = require('../database/dbConfig')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,12 +13,29 @@ module.exports = server => {
 };
 
 function register(req, res) {
- 
+ let user = req.body;
+const hash = bcrypt.hashSync(user.password, 10)
+user.password = hash;
+db('users')
+.insert(user)
+.then(ids => {
+  const id = ids[0]
+  db('users')
+  .where({id})
+  .then(users => {
+    res.status(201).json(users)
+  })
+})
+.catch(error => {
+  res.status(500).json(error)
+})
 }
 
 function login(req, res) {
  
 }
+
+
 
 function getJokes(req, res) {
   const requestOptions = {
